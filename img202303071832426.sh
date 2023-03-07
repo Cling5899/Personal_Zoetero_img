8c2be74e61ed286c9b621872804b8456cfa5cd83
Cling5899 <cling5899@qq.com>
2023-03-07 18:32:13 +0800

    Upload by PicGo

---
+++ img202303071832426.sh
@@ -0,0 +1,110 @@
+mv CRR290921_host_removed.1 CRR290921_host_removed_R1.fastq.gz
+mv CRR290921_host_removed.2 CRR290921_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290932_f1.gz -2 CRR290932_r2.gz --un-conc-gz CRR290932_host_removed > CRR290932_mapped_and_unmapped.sam
+mv CRR290932_host_removed.1 CRR290932_host_removed_R1.fastq.gz
+mv CRR290932_host_removed.2 CRR290932_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290931_f1.gz -2 CRR290931_r2.gz --un-conc-gz CRR290931_host_removed > CRR290931_mapped_and_unmapped.sam
+mv CRR290931_host_removed.1 CRR290931_host_removed_R1.fastq.gz
+mv CRR290931_host_removed.2 CRR290931_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290888_f1.gz -2 CRR290888_r2.gz --un-conc-gz CRR290888_host_removed > CRR290888_mapped_and_unmapped.sam
+mv CRR290888_host_removed.1 CRR290888_host_removed_R1.fastq.gz
+mv CRR290888_host_removed.2 CRR290888_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290863_f1.gz -2 CRR290863_r2.gz --un-conc-gz CRR290863_host_removed > CRR290863_mapped_and_unmapped.sam
+mv CRR290863_host_removed.1 CRR290863_host_removed_R1.fastq.gz
+mv CRR290863_host_removed.2 CRR290863_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290955_f1.gz -2 CRR290955_r2.gz --un-conc-gz CRR290955_host_removed > CRR290955_mapped_and_unmapped.sam
+mv CRR290955_host_removed.1 CRR290955_host_removed_R1.fastq.gz
+mv CRR290955_host_removed.2 CRR290955_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290889_f1.gz -2 CRR290889_r2.gz --un-conc-gz CRR290889_host_removed > CRR290889_mapped_and_unmapped.sam
+mv CRR290889_host_removed.1 CRR290889_host_removed_R1.fastq.gz
+mv CRR290889_host_removed.2 CRR290889_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290939_f1.gz -2 CRR290939_r2.gz --un-conc-gz CRR290939_host_removed > CRR290939_mapped_and_unmapped.sam
+mv CRR290939_host_removed.1 CRR290939_host_removed_R1.fastq.gz
+mv CRR290939_host_removed.2 CRR290939_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290981_f1.gz -2 CRR290981_r2.gz --un-conc-gz CRR290981_host_removed > CRR290981_mapped_and_unmapped.sam
+mv CRR290981_host_removed.1 CRR290981_host_removed_R1.fastq.gz
+mv CRR290981_host_removed.2 CRR290981_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290866_f1.gz -2 CRR290866_r2.gz --un-conc-gz CRR290866_host_removed > CRR290866_mapped_and_unmapped.sam
+mv CRR290866_host_removed.1 CRR290866_host_removed_R1.fastq.gz
+mv CRR290866_host_removed.2 CRR290866_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290965_f1.gz -2 CRR290965_r2.gz --un-conc-gz CRR290965_host_removed > CRR290965_mapped_and_unmapped.sam
+mv CRR290965_host_removed.1 CRR290965_host_removed_R1.fastq.gz
+mv CRR290965_host_removed.2 CRR290965_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290878_f1.gz -2 CRR290878_r2.gz --un-conc-gz CRR290878_host_removed > CRR290878_mapped_and_unmapped.sam
+mv CRR290878_host_removed.1 CRR290878_host_removed_R1.fastq.gz
+mv CRR290878_host_removed.2 CRR290878_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290882_f1.gz -2 CRR290882_r2.gz --un-conc-gz CRR290882_host_removed > CRR290882_mapped_and_unmapped.sam
+mv CRR290882_host_removed.1 CRR290882_host_removed_R1.fastq.gz
+mv CRR290882_host_removed.2 CRR290882_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290948_f1.gz -2 CRR290948_r2.gz --un-conc-gz CRR290948_host_removed > CRR290948_mapped_and_unmapped.sam
+mv CRR290948_host_removed.1 CRR290948_host_removed_R1.fastq.gz
+mv CRR290948_host_removed.2 CRR290948_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290974_f1.gz -2 CRR290974_r2.gz --un-conc-gz CRR290974_host_removed > CRR290974_mapped_and_unmapped.sam
+mv CRR290974_host_removed.1 CRR290974_host_removed_R1.fastq.gz
+mv CRR290974_host_removed.2 CRR290974_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290924_f1.gz -2 CRR290924_r2.gz --un-conc-gz CRR290924_host_removed > CRR290924_mapped_and_unmapped.sam
+mv CRR290924_host_removed.1 CRR290924_host_removed_R1.fastq.gz
+mv CRR290924_host_removed.2 CRR290924_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290886_f1.gz -2 CRR290886_r2.gz --un-conc-gz CRR290886_host_removed > CRR290886_mapped_and_unmapped.sam
+mv CRR290886_host_removed.1 CRR290886_host_removed_R1.fastq.gz
+mv CRR290886_host_removed.2 CRR290886_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290864_f1.gz -2 CRR290864_r2.gz --un-conc-gz CRR290864_host_removed > CRR290864_mapped_and_unmapped.sam
+mv CRR290864_host_removed.1 CRR290864_host_removed_R1.fastq.gz
+mv CRR290864_host_removed.2 CRR290864_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290871_f1.gz -2 CRR290871_r2.gz --un-conc-gz CRR290871_host_removed > CRR290871_mapped_and_unmapped.sam
+mv CRR290871_host_removed.1 CRR290871_host_removed_R1.fastq.gz
+mv CRR290871_host_removed.2 CRR290871_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290880_f1.gz -2 CRR290880_r2.gz --un-conc-gz CRR290880_host_removed > CRR290880_mapped_and_unmapped.sam
+mv CRR290880_host_removed.1 CRR290880_host_removed_R1.fastq.gz
+mv CRR290880_host_removed.2 CRR290880_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290951_f1.gz -2 CRR290951_r2.gz --un-conc-gz CRR290951_host_removed > CRR290951_mapped_and_unmapped.sam
+mv CRR290951_host_removed.1 CRR290951_host_removed_R1.fastq.gz
+mv CRR290951_host_removed.2 CRR290951_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290920_f1.gz -2 CRR290920_r2.gz --un-conc-gz CRR290920_host_removed > CRR290920_mapped_and_unmapped.sam
+mv CRR290920_host_removed.1 CRR290920_host_removed_R1.fastq.gz
+mv CRR290920_host_removed.2 CRR290920_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290879_f1.gz -2 CRR290879_r2.gz --un-conc-gz CRR290879_host_removed > CRR290879_mapped_and_unmapped.sam
+mv CRR290879_host_removed.1 CRR290879_host_removed_R1.fastq.gz
+mv CRR290879_host_removed.2 CRR290879_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290929_f1.gz -2 CRR290929_r2.gz --un-conc-gz CRR290929_host_removed > CRR290929_mapped_and_unmapped.sam
+mv CRR290929_host_removed.1 CRR290929_host_removed_R1.fastq.gz
+mv CRR290929_host_removed.2 CRR290929_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290950_f1.gz -2 CRR290950_r2.gz --un-conc-gz CRR290950_host_removed > CRR290950_mapped_and_unmapped.sam
+mv CRR290950_host_removed.1 CRR290950_host_removed_R1.fastq.gz
+mv CRR290950_host_removed.2 CRR290950_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290958_f1.gz -2 CRR290958_r2.gz --un-conc-gz CRR290958_host_removed > CRR290958_mapped_and_unmapped.sam
+mv CRR290958_host_removed.1 CRR290958_host_removed_R1.fastq.gz
+mv CRR290958_host_removed.2 CRR290958_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290960_f1.gz -2 CRR290960_r2.gz --un-conc-gz CRR290960_host_removed > CRR290960_mapped_and_unmapped.sam
+mv CRR290960_host_removed.1 CRR290960_host_removed_R1.fastq.gz
+mv CRR290960_host_removed.2 CRR290960_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290937_f1.gz -2 CRR290937_r2.gz --un-conc-gz CRR290937_host_removed > CRR290937_mapped_and_unmapped.sam
+mv CRR290937_host_removed.1 CRR290937_host_removed_R1.fastq.gz
+mv CRR290937_host_removed.2 CRR290937_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290927_f1.gz -2 CRR290927_r2.gz --un-conc-gz CRR290927_host_removed > CRR290927_mapped_and_unmapped.sam
+mv CRR290927_host_removed.1 CRR290927_host_removed_R1.fastq.gz
+mv CRR290927_host_removed.2 CRR290927_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290966_f1.gz -2 CRR290966_r2.gz --un-conc-gz CRR290966_host_removed > CRR290966_mapped_and_unmapped.sam
+mv CRR290966_host_removed.1 CRR290966_host_removed_R1.fastq.gz
+mv CRR290966_host_removed.2 CRR290966_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290936_f1.gz -2 CRR290936_r2.gz --un-conc-gz CRR290936_host_removed > CRR290936_mapped_and_unmapped.sam
+mv CRR290936_host_removed.1 CRR290936_host_removed_R1.fastq.gz
+mv CRR290936_host_removed.2 CRR290936_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290873_f1.gz -2 CRR290873_r2.gz --un-conc-gz CRR290873_host_removed > CRR290873_mapped_and_unmapped.sam
+mv CRR290873_host_removed.1 CRR290873_host_removed_R1.fastq.gz
+mv CRR290873_host_removed.2 CRR290873_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290957_f1.gz -2 CRR290957_r2.gz --un-conc-gz CRR290957_host_removed > CRR290957_mapped_and_unmapped.sam
+mv CRR290957_host_removed.1 CRR290957_host_removed_R1.fastq.gz
+mv CRR290957_host_removed.2 CRR290957_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290933_f1.gz -2 CRR290933_r2.gz --un-conc-gz CRR290933_host_removed > CRR290933_mapped_and_unmapped.sam
+mv CRR290933_host_removed.1 CRR290933_host_removed_R1.fastq.gz
+mv CRR290933_host_removed.2 CRR290933_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290874_f1.gz -2 CRR290874_r2.gz --un-conc-gz CRR290874_host_removed > CRR290874_mapped_and_unmapped.sam
+mv CRR290874_host_removed.1 CRR290874_host_removed_R1.fastq.gz
+mv CRR290874_host_removed.2 CRR290874_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290943_f1.gz -2 CRR290943_r2.gz --un-conc-gz CRR290943_host_removed > CRR290943_mapped_and_unmapped.sam
+mv CRR290943_host_removed.1 CRR290943_host_removed_R1.fastq.gz
+mv CRR290943_host_removed.2 CRR290943_host_removed_R2.fastq.gz
+bowtie2 -p 40 -x /home/hanmz/database/hg38/hg38 -1 CRR290883_f1.gz -2 CRR290883_r2.gz --un-conc-gz CRR290883_host_removed > CRR290883_mapped_and_unmapped.sam
+mv CRR290883_host_removed.1 CRR290883_host_removed_R1.fastq.gz
+mv CRR290883_host_removed.2 CRR290883_host_removed_R2.fastq.gz
